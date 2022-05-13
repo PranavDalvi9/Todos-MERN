17dd23bda177ff1682d080bd7d23f5cfd90fc0ed
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-13 14:00:34 +0530

    Home page and navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Routes from './Components/Routing/Routing';
 import Routing from './Components/Routing/Routing';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routing/>
     </div>
   );
