e9e77d1025f0916364803a9504ec65dffb4595e2
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-13 13:12:15 +0530

    Folder Structure created

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const connect = require("./src/configs/db")
+let port = process.env.PORT || 2344;
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+app.use(cors());
+
+// const flightController = require("./src/controllers/flightController")
+
+// const airportController = require("./src/controllers/airportController")
+ 
+// app.use("/flight", flightController)
+// app.use("/airport", airportController)
+
+
+
+
+app.listen(port, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("listening on port 2345");
+});
