24dd080f0dd5e4f2027dac97d65f37fe700877d1
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-13 13:47:41 +0530

    Home Page completed

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -3,8 +3,57 @@ import "./HomePage.css"
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>home</h1>
+    <div className='HomePage'>
+
+
+      <div className='MainTodo' >
+
+        {/* Todo div start */}
+        <div>
+          <div><p>To DO</p></div>
+
+          <div className='Card'>
+            <div className='CardTitle'><p>Title</p></div>
+            <div className='CardDescription'><p>Description: temporibus officia rem tempore officiis.</p></div>
+            <div className='CardButton'>
+              <button>Doing</button>
+              <button>Done</button>
+            </div>
+          </div>
+
+        </div>
+
+        {/* Doing div start */}
+        <div>
+          <div><p>Doing</p></div>
+
+          <div className='Card'>
+            <div className='CardTitle'><p>Title</p></div>
+            <div className='CardDescription'><p>Description: temporibus officia rem tempore officiis.</p></div>
+            <div className='CardButton'>
+              {/* <button>Doing</button> */}
+              <button>Done</button>
+            </div>
+          </div>
+
+        </div>
+
+        {/* Done div start */}
+        <div>
+          <div><p>Done</p></div>
+
+          <div className='Card'>
+            <div className='CardTitle'><p>Title</p></div>
+            <div className='CardDescription'><p>Description: temporibus officia rem tempore officiis.</p></div>
+            {/* <div className='CardButton'>
+              <button>Doing</button>
+              <button>Done</button>
+            </div> */}
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
